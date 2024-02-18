7b332cd7c99b287b4854eb8a4b58fda66262fb00
dotrangtuan <dotrangtuan02@gmail.com>
2024-02-18 16:42:37 +0700

    init

---
+++ chamcong/src/component/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './headerStyle.css'
+
+export default function Header() {
+  return (
+    <>
+      <div className='title'>Đồ án tốt nghiệp 2024 - Hệ thống chấm công nhân viên</div>
+      <div className='nav'></div>
+    </>
+    
+  )
+}
